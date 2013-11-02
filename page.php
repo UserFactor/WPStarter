021f04e188e1f2f869b6e00a492a5326cc52cc09
Humberto Suastegui <hs@suastex.com>
2013-11-02 21:18:40 +0000

    c1

--- page.php
+++ page.php
@@ -1,18 +1,10 @@
-<?php
-/**
- * @package WordPress
- * @subpackage HTML5-Reset-WordPress-Theme
- * @since HTML5 Reset 2.0
- */
- get_header(); ?>
-
+<?php get_header(); ?>
+<section role="main">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 		<article class="post" id="post-<?php the_ID(); ?>">
 
 			<h2><?php the_title(); ?></h2>
-
-			<?php posted_on(); ?>
 
 			<div class="entry">
 
@@ -22,14 +14,8 @@
 
 			</div>
 
-			<?php edit_post_link(__('Edit this entry.'), '<p>', '</p>'); ?>
-
 		</article>
-		
-		<?php comments_template(); ?>
-
-		<?php endwhile; endif; ?>
-
+	<?php endwhile; endif; ?>
+</section>
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
--- pagination.php
+++ pagination.php
@@ -1,5 +1,4 @@
-<!-- pagination -->
-<div class="pagination">
-	<?php html5wp_pagination(); ?>
+<div class="navigation">
+	<div class="next-posts"><?php next_posts_link('&laquo; Older Entries') ?></div>
+	<div class="prev-posts"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 </div>
-<!-- /pagination -->
