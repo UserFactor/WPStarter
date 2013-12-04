ac5208ec3b4c7ec3f54e8e658d4a29ec733aaef2
Humberto Suastegui <hsuastegui@gmail.com>
2013-12-04 11:38:11 +0000

    Update header.php

--- header.php
+++ header.php
@@ -12,17 +12,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
-    <meta name="Copyright" content="Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved.">
+    	<meta name="Copyright" content="Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved.">
     
-    <title><?php //wp_title( '|', true, 'right' ); ?></title>
-    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+    	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
 	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
     
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" />
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
-	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php wp_head(); ?>
