0d54c09fefe4dfb33235d148e4b1ba10daa47c44
Humberto Suastegui <hsuastegui@gmail.com>
2013-12-04 11:49:14 +0000

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,9 @@
 <aside class="sidebar" role="complementary">
+
+<?php wp_nav_menu( array('menu' => 'side') ); ?>
+
 <?php if ( is_active_sidebar('sidebar-widget') ) : ?>
     <?php dynamic_sidebar('sidebar-widget') ?>
 <?php endif; ?>
+
 </aside>
