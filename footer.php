2997dc76c3885799ef5f5afa91615ff589e6da1a
Humberto Suastegui <hsuastegui@gmail.com>
2013-12-04 11:42:11 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
-
 		<footer class="mainfooter" role="contentinfo">
 			<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></small>
 		</footer>
 
 	</div><!-- close wrapper -->
 <?php wp_footer(); ?>
-<script src="<?php bloginfo('template_directory'); ?>/js/functions.js"></script>
 </body>
 </html>
