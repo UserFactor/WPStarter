2f446aa8f9e6612e8280f1671310777f15a1bd0a
Humberto Suastegui <hsuastegui@gmail.com>
2013-12-04 12:08:06 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
 		<footer class="mainfooter" role="contentinfo">
+		
+		<?php if ( is_active_sidebar('footer-widget') ) : ?>
+    			<?php dynamic_sidebar('footer-widget') ?>
+		<?php endif; ?>
+		
 			<small>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></small>
 		</footer>
 
